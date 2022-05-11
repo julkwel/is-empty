8d15c140296c28f6bbd3e18207f1f429bf37915d
julkwel <julienrajerison5@gmail.com>
2022-05-11 17:57:20 +0300

    [test] :  add testing and some improvement

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-    isEmpty
-}
-
-function isEmpty($value) {
-    return !$value || (0 === $value.trim().length) || (!isNaN(parseInt($value)) && 0 === parseInt($value));
-}
